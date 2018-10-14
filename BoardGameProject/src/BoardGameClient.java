b58272974874589146f7b26a826ca8a6aa3d6e12
abargum <abargu17@student.aau.dk>
2018-10-14 14:39:01 +0200

    Client class started, most of the basics has been added - current functionality is just an example to secure communication

--- BoardGameProject/src/BoardGameClient.java
+++ BoardGameProject/src/BoardGameClient.java
@@ -1,10 +1,72 @@
+/*import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;*/
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class BoardGameClient 
 {
-
+  
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		
+		//In case the user needs to input, the scanner is declared 
+		Scanner input = new Scanner(System.in);	
+		//This boolean makes sure the communication between the client and server only
+		//takes place when actually connected
+		boolean connect = true;
+
+		try {
+			 
+			// Create a socket to connect to the server
+			  Socket connectToServer = new Socket("localhost", 7845);
+			  
+			  // Create an input stream to receive data from the server
+			  DataInputStream isFromServer = new DataInputStream(connectToServer.getInputStream());
+			  
+			// Create an output stream to send data to the server
+			  DataOutputStream osToServer = new DataOutputStream(connectToServer.getOutputStream());
+			  
+			  while (connect) {
+				  
+				  //Ask the client if it is ready
+				  System.out.print("Welcome to this awesome board game - type: 'yes' to start and 'no' to exit");
+				  if (input.next().equals("no")) {	
+					  System.out.print("You have exited the game");	
+					  break;	
+				  }
+				  
+				  //The client rolls the dice by using the dice class and receives a random number
+				  System.out.print("Please roll the dice (until now just enter 2)");
+				  int diceRoll = input.nextInt(); 
+				  
+				  //Send the diceroll to the server
+				  osToServer.writeInt(diceRoll);
+				  
+				 //flushes all the streams of data and executes them completely 
+				  //and gives a new space to new streams 
+				  osToServer.flush();
+				  
+				  //Get updated score from server
+				  double clientScore = isFromServer.readInt();
+				  
+				  //print out score
+				  System.out.println("Your new score is: " + clientScore);
+			 
+			  }  input.close();
+				connectToServer.close();
+			  
+		} catch (IOException ex) {
+			System.out.println(ex.toString() + '\n');
+		}
+		
+	}
+}
+				
+			  // TODO Auto-generated method stub
 		
 		/*THE CLIENT HAS TO:
 		 * -CONNECT TO THE SERVER
@@ -23,6 +85,3 @@ public class BoardGameClient
 		 * 
 		 * */
 
-	}
-
-}
